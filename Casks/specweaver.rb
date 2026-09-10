df7801d971af31eee0a0817c3014d1e2c9787109
SpecWeaver <specweaver-release@users.noreply.github.com>
2026-09-10 03:11:37 +0000

    specweaver 0.1.7

--- Casks/specweaver.rb
+++ Casks/specweaver.rb
@@ -1,6 +1,6 @@
 cask "specweaver" do
-  version "0.1.6"
-  sha256 "6362dd44435bd0da8b386bd2543216153dbb579c06905fee5541f06a8283e4e4"
+  version "0.1.7"
+  sha256 "d422e0f3e9e1c387b9c221609289469ecc770eaf2168d6b4e8f0447a7485f956"
 
   url "https://github.com/yangfanfengshun/SpecWeaver-App/releases/download/v#{version}/SpecWeaver_#{version}_universal.dmg"
   name "SpecWeaver"
